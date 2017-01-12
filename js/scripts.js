0c68b5c74cdf6b5fc20e1ff08e99819602f56ff0
jb <jenniferbeem@gmail.com>
2017-01-12 12:09:05 -0800

    add initial files for project and create if-else statement function for political affiliation prediction

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
-  var age = parseInt(prompt("How old are you?"));
+  // var age = parseInt(prompt("How old are you?"));
+  //
+  // if (age >= 18) {
+  //   $('#Over').show();
+  // } else {
+  //   $('#Under').show();
+  // });
 
-  if (age >= 18) {
-    $('#Over').show();
-  } else {
-    $('#Under').show();
-  }
+  $("form#politics").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+
+    if (age > 35 || gender === "male") {
+      $("#party").append("conservative");
+    } else {
+      $("#party").append("liberal");
+    }
+
+      $("#prediction").show();
+
+    event.preventDefault();
+  });
 });
